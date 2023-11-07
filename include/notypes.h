577b46dfefd9d0f49ed33e368766e2d061161069
Momtchil Momtchev <momtchil@momtchev.com>
2023-11-07 10:32:35 +0100

    clarify the rules

--- include/notypes.h
+++ include/notypes.h
@@ -23,12 +23,23 @@ template <class C, typename T> T getMemberPointerType(T C::*v);
 
 namespace Typemap {
 
-// Typemap::FromJS rules
-// - The constructor should check the incoming value
-// - operator* should return an in-place constructed prvalue
-// - operator* can be called in a background thread - no V8 Local<>s allowed
-// - The constructor can create state that will be destroyed after the function call
+/**
+ * Typemap::FromJS rules
+ * - The constructor should check the incoming value
+ * - The constructor is called on the V8 main thread
+ * - operator* should return an in-place constructed prvalue
+ * - operator* can be called in a background thread - no V8 Local<>s allowed
+ * - The constructor can create state that will be destroyed after the function call
+ */
 template <typename T> class FromJS;
+
+/**
+ * Typemap::ToJS rules
+ * - The constructor will be called in a background thread
+ * - operator* should return an in-place constructed prvalue
+ * - operator* can be called on the main V8 thread -> Local<>s allowed
+ * - The constructor cannot create state
+ */
 template <typename T, const ReturnAttribute &RETATTR> class ToJS;
 
 template <const ReturnAttribute &RETATTR> class ToJS<bool, RETATTR> {
