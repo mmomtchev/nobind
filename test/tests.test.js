359aa7134a9e5098c7a543f3658e2499e672a5f9
Momtchil Momtchev <momtchil@momtchev.com>
2023-12-21 11:36:21 +0100

    do this correctly

--- test/tests.test.js
+++ test/tests.test.js
@@ -5,11 +5,13 @@ describe('nobind', function () {
   for (const t of tests) {
     describe(t, () => {
       framework.register(t);
-      try {
-        before('clean', () => framework.clean(t));
-      } catch (e) {
-        console.log('cleaning the build failed, module not unloaded on Windows?');
-      }
+      before('clean', () => {
+        try {
+          framework.clean(t);
+        } catch (e) {
+          console.log('cleaning the build failed, module not unloaded on Windows?');
+        }
+      });
       before('configure', () => framework.configure(t, undefined, process.env.ENABLE_ASAN && ['--debug', '--enable_asan']));
       before('build', () => framework.build());
       before('load', () => framework.load(t, process.env.ENABLE_ASAN && 'Debug'));
