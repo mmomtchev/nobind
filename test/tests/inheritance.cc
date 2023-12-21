c69ba0029e853c10d90b30f15a23af576ab5c13d
Momtchil Momtchev <momtchil@momtchev.com>
2023-12-21 15:26:49 +0100

    reformat

--- test/tests/inheritance.cc
+++ test/tests/inheritance.cc
@@ -8,13 +8,13 @@ NOBIND_MODULE(inheritance, m) {
   m.def<Derived>("Derived")
       .cons<int>()
       .def<&Derived::get>("get")
+  // Both of these are artificial edge cases for testing purposes
+  // and MSVC 2019 is already pushed to its limits, we choose to certify
+  // it without them as they are not really needed
 #if !defined(_MSC_VER) || _MSC_VER >= 1930
       // Get explicitly the address to the virtual method in the Base class
       // The C++ templating will still invoke the correct method as the
       // invocation happens through a Derived object
-      // (both of these are artificial edge cases for testing purposes
-      // and MSVC 2019 is already pushed to its limits, we choose to certify
-      // it without them as they are not really needed)
       .def<&Base::get>("virtual_base_get")
       .def<&Derived::base_get>("base_get")
 #endif
