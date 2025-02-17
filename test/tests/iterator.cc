e6ee933fdd9546282943f0a072593379ebef66af
Momtchil Momtchev <momtchil@momtchev.com>
2025-02-17 17:36:21 +0100

    add a warning

--- test/tests/iterator.cc
+++ test/tests/iterator.cc
@@ -11,7 +11,7 @@ template <typename T> class JSIterator {
 public:
   JSIterator(T &obj, typename T::iterator begin) : target(obj), it(begin) {}
   Napi::Value next(Napi::Env env) {
-    using value_type_t = std::remove_reference_t<decltype(*it)>;
+    using value_type_t = typename decltype(it)::value_type;
 
     Napi::Object ret = Napi::Object::New(env);
     if (it == target.end()) {
@@ -19,8 +19,14 @@ public:
     } else {
       Napi::Value value;
       if constexpr (std::is_scalar_v<value_type_t>) {
-        value = Nobind::ToJS<value_type_t, Nobind::ReturnShared>(env, *it).Get();
+        // Scalar types are simply copied
+        value = Nobind::ToJS<value_type_t, Nobind::ReturnDefault>(env, *it).Get();
       } else {
+        // Non-scalar types are returned as a shared reference
+        // This assumes that their container won't get destroyed
+        // If the object is copy-constructible the safest option is to copy it
+        // Otherwise, special care must be taken that these objects contain a reference
+        // to their container
         value = Nobind::ToJS<value_type_t &, Nobind::ReturnShared>(env, *it).Get();
       }
       ret.Set("value", value);
