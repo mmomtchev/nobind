f2933489824a581724f805dd8d288b9646978264
Momtchil Momtchev <momtchil@momtchev.com>
2025-02-17 18:41:01 +0100

    this will probably stay as an example

--- test/tests/iterator.cc
+++ test/tests/iterator.cc
@@ -4,6 +4,9 @@
 
 // This is the tricky part
 // This is a JS iterator
+// You can use it as an example as to how to implement your own iterators
+// It is generic enough to be directly usable with all types,
+// but you must understand the implications of returning references to JS
 template <typename T> class JSIterator {
   T &target;
   typename T::iterator it;
@@ -21,12 +24,13 @@ public:
       if constexpr (std::is_scalar_v<value_type_t>) {
         // Scalar types are simply copied
         value = Nobind::ToJS<value_type_t, Nobind::ReturnDefault>(env, *it).Get();
-      } else {
-        // Non-scalar types are returned as a shared reference
-        // This assumes that their container won't get destroyed
+      } else if constexpr (std::is_copy_constructible_v<value_type_t>) {
         // If the object is copy-constructible the safest option is to copy it
-        // Otherwise, special care must be taken that these objects contain a reference
-        // to their container
+        value = Nobind::ToJS<value_type_t, Nobind::ReturnDefault>(env, *it).Get();
+      } else {
+        // Other objects are returned as a shared reference
+        // This assumes that their container won't get destroyed
+        // Note that ReturnNested won't work here - it is valid only for class members
         value = Nobind::ToJS<value_type_t &, Nobind::ReturnShared>(env, *it).Get();
       }
       ret.Set("value", value);
@@ -40,6 +44,8 @@ public:
 using Iterable = Range<10, 20>;
 
 // Helper to construct it
+// MSVC 2019 is not fully C++17 compliant and has problems with using
+// pointers to templated functions as non-type template arguments
 #if !defined(_MSC_VER) || _MSC_VER >= 1930
 template <typename T> JSIterator<T> IteratorWrapper(T &obj) { return JSIterator<T>{obj, obj.begin()}; }
 #else
