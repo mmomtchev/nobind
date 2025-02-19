b01f3625cf760f380cd8402f531beb10446d413b
Momtchil Momtchev <momtchil@momtchev.com>
2025-02-19 16:18:27 +0100

    more debugging

--- include/nonapi.h
+++ include/nonapi.h
@@ -14,16 +14,28 @@
 #error C++17 is required to build with nobind17
 #endif
 
-#include <napi.h>
-
 #if defined(NODE_ADDON_API_REQUIRE_BASIC_FINALIZERS)
-#warning require basic finalizers is enabled
+#warning before napi require basic finalizers enabled
 #endif
 
 #if defined(NODE_API_EXPERIMENTAL_HAS_POST_FINALIZER)
-#warning post finalizer enabled
+#warning before napi post finalizer enabled
 #endif
 
 #if defined(NAPI_EXPERIMENTAL)
-#warning experimental enabled
+#warning before napi experimental enabled
+#endif
+
+#include <napi.h>
+
+#if defined(NODE_ADDON_API_REQUIRE_BASIC_FINALIZERS)
+#warning after napi require basic finalizers enabled
+#endif
+
+#if defined(NODE_API_EXPERIMENTAL_HAS_POST_FINALIZER)
+#warning after napi post finalizer enabled
+#endif
+
+#if defined(NAPI_EXPERIMENTAL)
+#warning after napi experimental enabled
 #endif
