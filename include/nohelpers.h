5f8191d32214c5f0c658788ecdd37fadfad1be81
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-03 13:53:15 +0200

    workaround the env shutdown quirk

--- include/nohelpers.h
+++ include/nohelpers.h
@@ -47,10 +47,21 @@ template <typename T> void InitMainThreadQueue(Napi::Env env) {
  * ---------------------------------------------------------------------------*/
 template <typename T> void RunOnJSMainThread(Napi::BasicEnv env, std::function<void()> &&job) {
   auto env_data = env.GetInstanceData<T>();
+  if (!env_data) {
+    // This is a very annoying quirk of Node.js/V8 - the
+    // GC can run after the environment has been destroyed
+    // Luckily, the behavior of Napi::Env::GetInstanceData
+    // seems to be well defined - it returns nullptr
+    NOBIND_VERBOSE(OBJECT, "Skipping a finalizer because the environment has been shut down\n");
+    return;
+  }
   if (env_data->_Nobind_js_thread == std::this_thread::get_id()) {
     // Run synchronously when called on the main thread
     job();
   } else {
+    // Normally the environment cannot be destroyed
+    // with a waiting open uv_async, if was still alive above
+    // it will still be alive when the tasklets run
     std::lock_guard<std::mutex> lock(env_data->_Nobind_js_thread_jobs_lock);
     env_data->_Nobind_js_thread_jobs.emplace(std::move(job));
     if (uv_async_send(&env_data->_Nobind_js_thread_async_handle) != 0)
