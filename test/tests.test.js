2c51c439fb867791c0da6f90d1a5ee0173a7b311
Momtchil Momtchev <momtchil@momtchev.com>
2025-11-14 11:35:46 +0100

    add a debug build to CI

--- test/tests.test.js
+++ test/tests.test.js
@@ -3,11 +3,15 @@ const { mocha_typescript } = require('./opts');
 
 describe('nobind', function () {
   const tests = framework.list();
+  const debug = !!process.env.ENABLE_DEBUG || !!process.env.ENABLE_ASAN;
+  const build_opts = [];
+  if (debug) build_opts.push('--debug');
+  if (process.env.ENABLE_ASAN) build_opts.push('--enable_asan');
   for (const t of tests) {
     describe(t, () => {
-      before('configure', () => framework.configure(t, undefined, process.env.ENABLE_ASAN && ['--debug', '--enable_asan'] || []));
+      before(`configure ${build_opts.join(' ')}`, () => framework.configure(t, undefined, build_opts));
       before('build', () => framework.build());
-      before('load', () => framework.load(t, process.env.ENABLE_ASAN && 'Debug'));
+      before('load', () => framework.load(t, debug ? 'Debug' : 'Release'));
       if (mocha_typescript()) {
         before('generate types', () => framework.gen_typescript());
         it('check types', () => framework.check_typescript(t));
